27553dbadd3939568ee7f0a33a311947a02048d6
Ashish Singh <159794249+iashishkr07@users.noreply.github.com>
2025-05-19 00:45:54 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  FullName: { type: String, required: true,},
-  Email: { type: String, required: true, unique: true, },
-  Phone: { type: String, required: true, default:'0000000000' },
-  Password: { type: String, required: true, },
-});
+const userSchema = new mongoose.Schema(
+  {
+    FullName: { type: String, required: true },
+    Email: { type: String, required: true, unique: true },
+    Phone: { type: String },
+    Password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
-export default User;
+export default mongoose.model("User", userSchema);
