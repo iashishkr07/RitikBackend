182f4d65ea70795f6d3b185e59c56a199d4a45e4
Ashish Singh <159794249+iashishkr07@users.noreply.github.com>
2025-06-25 02:56:47 +0530

    Update registerModel.js

--- models/registerModel.js
+++ models/registerModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
+const registerSchema = new mongoose.Schema(
   {
     Name: { type: String, required: true },
     Email: { type: String, required: true, unique: true },
@@ -10,4 +10,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("Register", registerSchema)
